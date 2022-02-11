a1cbaf0e88a2e8d7d155a8d5a17f5764be060de2
Felix Mark <felix.mark@hotmail.com>
2022-02-11 12:53:31 +0100

    Added new features to ATTiny program.

--- 0.2/Software/AT_Tiny_Program/AT_Tiny_Program.ino
+++ 0.2/Software/AT_Tiny_Program/AT_Tiny_Program.ino
@@ -42,7 +42,6 @@
 #define NUM_ELEMENTS        105
 #define SERIAL_TIMEFRAME    5000
 #define SERIAL_BUFFER_SIZE  30
-#define ISR_CNT_TO_WAKE_UP  2
 
 // Pins
 #define PIN_LED             PCINT4
@@ -78,20 +77,21 @@ static uint8_t SIGMOID [NUM_ELEMENTS] = {
 Adafruit_NeoPixel led = Adafruit_NeoPixel(1, PIN_LED, NEO_GRBW + NEO_KHZ800);
 SoftwareSerial mySerial(PIN_RX, PIN_TX);
 
-volatile bool awake = false;
-uint8_t current_sleep_cycles = 1; // Between 1 and 10 cycles = 8 to 80 seconds sleep
-int8_t sleep_cycle_counter = 1;
-uint8_t isr_cnt = 0;              // Short blink every 8 * isr_cnt seconds
-int soc = 0;
-uint8_t serial_process_position = 0;
+volatile bool awake = false;          // Used for waking up in ISR (Interrupt Service Routine)
+uint8_t current_sleep_cycles = 1;     // Between 1 and 10 cycles = 8 to 80 seconds sleep
+int8_t sleep_cycle_counter = 1;       // Counting how often uC woke up and blinked shortly
+uint8_t isr_cnt = 0;                  // Short blink every 8 * isr_cnt seconds
+uint8_t isr_cnt_to_wake_up = 2;       // How many interrupts needed to wake up (isr_cnt_to_wake_up * 8s)
+int soc = 0;                          // Store SOC measurement (State of charge)
+uint8_t serial_process_position = 0;  // How much of the received serial string has been processed
 
-uint8_t mode = MODE_DEFAULT;
-uint8_t color_r = 255;
-uint8_t color_g = 0;
-uint8_t color_b = 80;
-uint8_t color_w = 100;
+uint8_t mode = MODE_DEFAULT;          // Store mode
+uint8_t color_r = 255;                // Store red value
+uint8_t color_g = 0;                  // Store green value
+uint8_t color_b = 80;                 // Store blue value
+uint8_t color_w = 100;                // Store white value
 
-uint8_t offsets[4] = {0,0,0,0};
+uint8_t offsets[4] = {0,0,0,0};       // Store calculated offsets for smoother animation
 
 // EEPROM
 uint8_t mode_eeprom EEMEM;
@@ -99,7 +99,8 @@ uint8_t color_r_eeprom EEMEM;
 uint8_t color_g_eeprom EEMEM;
 uint8_t color_b_eeprom EEMEM;
 uint8_t color_w_eeprom EEMEM;
-uint8_t color_set_eeprom EEMEM;
+uint8_t color_set_eeprom EEMEM;       // Save if color was ever set (if not, there will be a problem loading it!)
+uint8_t isr_cnt_to_wake_up_eeprom EEMEM;
 
 
 
@@ -114,13 +115,13 @@ void setup() {
     pinMode(PIN_LED_ENABLE, OUTPUT);
 
     // Setup WDT
-    cli();                  // Disable Interrupts
-    wdt_reset();            // Reset Watchdog Timer
-    MCUSR &= ~(1 << WDRF);  // Disable Watchdog System Reset
-    WDTCR = (1 << WDCE);    // Watchdog Change Enable
+    cli();                          // Disable Interrupts
+    wdt_reset();                    // Reset Watchdog Timer
+    MCUSR &= ~(1 << WDRF);          // Disable Watchdog System Reset
+    WDTCR = (1 << WDCE);            // Watchdog Change Enable
     WDTCR |= 1 << WDP0 | 1 << WDP3; // Set WDT timeout to 8 seconds
-    WDTCR |= (1 << WDIE);   // Watchdog Timeout Interrupt Enable
-    sei();                  // Enable Interrupts
+    WDTCR |= (1 << WDIE);           // Watchdog Timeout Interrupt Enable
+    sei();                          // Enable Interrupts
 
     // Restore variables from EEPROM if they are set
     uint8_t temp_mode = eeprom_read_byte(&mode_eeprom);
@@ -132,6 +133,10 @@ void setup() {
         color_g = eeprom_read_byte(&color_g_eeprom);
         color_b = eeprom_read_byte(&color_b_eeprom);
         color_w = eeprom_read_byte(&color_w_eeprom);
+    }
+    uint8_t temp_isr_cnt_to_wake_up = eeprom_read_byte(&isr_cnt_to_wake_up_eeprom);
+    if (temp_isr_cnt_to_wake_up != 0xFF) {
+        isr_cnt_to_wake_up = temp_isr_cnt_to_wake_up;
     }
 
     // Handle Serial communication
@@ -211,19 +216,18 @@ void loop() {
 
 // ============================================================= SERIAL COMMUNICATION
 void handle_serial() {
-    blink_led(0,0,0,30);
+    blink_led(0,0,30,0);
     mySerial.begin(SERIAL_BAUD);
     
     char recv_buffer[SERIAL_BUFFER_SIZE] = {};
     char character = 0;
 
     // Send current color for synchronizing Omamoris
-    mySerial.println("Omamori v" VERSION);
-    print_current_color();
+    mySerial.println("LUV");                // Used for pairing
 
     // Wait for incomming serial bytes
-    while (millis() < SERIAL_TIMEFRAME) {
-        // Read first line
+    while (millis() < SERIAL_TIMEFRAME) {   // While serial communication is enabled
+        // Read only first line received
         if (mySerial.available() > 0) {
             uint8_t pos = 0;
             serial_process_position = 0;
@@ -234,15 +238,16 @@ void handle_serial() {
                 delay(5);
             }
 
-            blink_led(0,50,0,0);
+            blink_led(0,0,0,30);
             delay(10);
             
-            mySerial.print("Received: ");
+            mySerial.print("ACK: ");
             for (uint8_t i = 0; i < pos; ++i) {
                 mySerial.print(recv_buffer[i]);
             }
             mySerial.println();
 
+            // COL command for setting color
             if (recv_buffer[0] == 'C' && recv_buffer[1] == 'O' && recv_buffer[2] == 'L') {
                 serial_process_position = 4;
                 
@@ -257,38 +262,74 @@ void handle_serial() {
                 update_eeprom(color_g_eeprom, color_g);
                 update_eeprom(color_b_eeprom, color_b);
                 update_eeprom(color_w_eeprom, color_w);
+                
+                print_okay();
             }
+            
+            // BAT command for setting battery display mode
             else if (recv_buffer[0] == 'B' && recv_buffer[1] == 'A' && recv_buffer[2] == 'T') {
                 mode = MODE_BATTERY;
                 update_eeprom(mode_eeprom, mode);
-                mySerial.println("Mode: Battery");
+                print_okay();
             }
+            
+            // LUV command for pairing
+            else if (recv_buffer[0] == 'L' && recv_buffer[1] == 'U' && recv_buffer[2] == 'V') {
+                delay(100);
+                print_current_color();
+            }
+            
+            // DEF command for setting default blinking mode
             else if (recv_buffer[0] == 'D' && recv_buffer[1] == 'E' && recv_buffer[2] == 'F') {
                 mode = MODE_DEFAULT;
                 update_eeprom(mode_eeprom, mode);
-                mySerial.println("Mode: Default");
+                print_okay();
             }
+            
+            // STA command for getting status
             else if (recv_buffer[0] == 'S' && recv_buffer[1] == 'T' && recv_buffer[2] == 'A') {
                 mySerial.println("Omamori v" VERSION);
                 read_soc();
                 mySerial.print("SOC: ");
                 mySerial.println(soc);
+                mySerial.print("MODE: ");
+                if (mode == MODE_DEFAULT) {
+                    mySerial.println("DEF");
+                }
+                if (mode == MODE_BATTERY) {
+                    mySerial.println("BAT");
+                }
+                mySerial.print("SLEEP: ");
+                mySerial.print(isr_cnt_to_wake_up * 8);
+                mySerial.println("s");
                 print_current_color();
+                mySerial.print("MODE: ");
             }
-            else if (recv_buffer[0] == 'F' && recv_buffer[1] == 'J' && recv_buffer[2] == 'M') {
-                mySerial.println("UwU");
-            }
-            else {
-                mySerial.println("Unknown command.");
+            
+            // SLP command for setting sleep time (value x 8s)
+            else if (recv_buffer[0] == 'S' && recv_buffer[1] == 'L' && recv_buffer[2] == 'P') {
+                serial_process_position = 4;
+                uint8_t tmp_isr_cnt_to_wake_up = get_value(recv_buffer, serial_process_position, ';');
+                if (tmp_isr_cnt_to_wake_up >= 1 && tmp_isr_cnt_to_wake_up <= 10) {
+                    isr_cnt_to_wake_up = tmp_isr_cnt_to_wake_up;
+                    update_eeprom(isr_cnt_to_wake_up_eeprom, isr_cnt_to_wake_up);
+                    print_okay();
+                }
+                else {
+                  print_error();
+                }
             }
 
-            // Serial Request has been processed. Print new line.
-            mySerial.println("");
+            // Unknown command
+            else {
+                print_error();
+            }
+
             break;
         }
     }
     
-    blink_led(0,0,0,30);
+    blink_led(30,0,0,0);
     mySerial.end();
 }
 
@@ -296,8 +337,9 @@ void handle_serial() {
 
 // WDT ISR
 ISR(WDT_vect) {
+    // Increase isr_cnt and wake up, when necessary
     isr_cnt += 1;
-    if (isr_cnt >= ISR_CNT_TO_WAKE_UP) {
+    if (isr_cnt >= isr_cnt_to_wake_up) {
         awake = true;
         isr_cnt = 0;
     }
@@ -305,13 +347,12 @@ ISR(WDT_vect) {
 
 // ENTER SLEEP
 void go_to_bed() {
-    wdt_reset();
-    sleep_cycle_counter -= 1;
-    awake = false;
+    wdt_reset();                        // Reset WDT
+    sleep_cycle_counter -= 1;           // Decrease sleep cycle counter, since one time is right now
+    awake = false;                      // uC is not awake anymore
 
     // Prepare for bed
-    byte adcsra;
-    adcsra = ADCSRA;                    // ADC Control and Status Register A sichern
+    byte adcsra = ADCSRA;               // ADC Control and Status Register A sichern
     ADCSRA &= ~(1 << ADEN);             // ADC ausschalten
     MCUCR |= (1 << SM1) & ~(1 << SM0);  // Sleep-Modus = Power Down
     MCUCR |= (1 << SE);                 // Sleep Enable setzen
@@ -326,26 +367,26 @@ void go_to_bed() {
 
 // SHOW COLOR
 void show_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t white) {
-    led.setPixelColor(0, red, green, blue, white);
-    led.show();
+    led.setPixelColor(0, red, green, blue, white);  // There is only one neopixel, so it is always at position 0
+    led.show();                                     // Show set color
 }
 
 // ENABLE LED
 void enable_led() {
     digitalWrite(PIN_LED_ENABLE, HIGH);
-    delay(10);
+    delay(10);                            // Short delay after enabling the LED, so it does not freak out
 }
 
 // DISABLE LED
 void disable_led() {
-    show_color(0,0,0,0);
-    delay(10);
-    digitalWrite(PIN_LED_ENABLE, LOW);
+    show_color(0,0,0,0);                  // Set color to off
+    delay(10);                            // Short delay, so the color (off) can be set
+    digitalWrite(PIN_LED_ENABLE, LOW);    // Disable Neopixel
 }
 
 // GET SOC
 void read_soc() {
-    soc = analogRead(PIN_MEASUREMENT);
+    soc = analogRead(PIN_MEASUREMENT);    // Write analog value from ADC to soc variable (0-1023)
 }
 
 // BLINK LED
@@ -354,6 +395,17 @@ void blink_led(uint8_t red, uint8_t green, uint8_t blue, uint8_t white) {
     show_color(red, green, blue, white);
     delay(BLINK_ON_TIME);
     disable_led();
+}
+
+
+// SEND SERIAL okay
+void print_okay() {
+    mySerial.println("OK");
+}
+
+// SEND SERIAL no
+void print_error() {
+    mySerial.println("ERROR");
 }
 
 // PRINT CURRENT COLOR
@@ -394,6 +446,7 @@ uint8_t get_value(char data[SERIAL_BUFFER_SIZE], uint8_t begin_pos, char end_cha
 
 // EETPROM FUNCTIONS
 void update_eeprom(uint8_t &variable, uint8_t value) {
+    // Only write to eeprom if value is not already set (so writing is not performed that often)
     if (eeprom_read_byte(&variable) != value) {
         eeprom_write_byte(&variable, value);
     }
